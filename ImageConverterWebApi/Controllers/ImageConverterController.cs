7364e6c0a6e3b2a3660df8d53520939ad35a4996
DSadykov <d.sadykov@inbox.ru>
2022-01-12 17:48:42 +0500

    fix1

--- ImageConverterWebApi/Controllers/ImageConverterController.cs
+++ ImageConverterWebApi/Controllers/ImageConverterController.cs
@@ -1,7 +1,7 @@
 using ImageConverterWebApi.Models;
 using ImageConverterWebApi.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Drawing;
+using Newtonsoft.Json;
 
 namespace ImageConverterWebApi.Controllers;
 
@@ -14,8 +14,12 @@ public class ImageConverterController : ControllerBase
     private readonly ImageConverterService _imageConverter;
     public ImageConverterController(ILogger<ImageConverterController> logger, ImageConverterService imageConverter)
     {
+        var tmp = new byte[] { 1, 2, 3, 5, 5, 7 };
+        var tmp2 = JsonConvert.SerializeObject(tmp);
+        logger.LogInformation(tmp2);
         _logger = logger;
         _imageConverter = imageConverter;
+        var tmo = Request;
     }
 
     [HttpPost(Name = "PostImage")]
@@ -24,9 +28,16 @@ public class ImageConverterController : ControllerBase
         return await _imageConverter.ConvertImage(imageModel);
     }
     //[HttpPost(Name = "PostImage")]
-    //public async Task<OutputImageModel> Post([FromForm] byte[] imageModel)
+    //public async Task<OutputImageModel> Post(string imageModel)
     //{
-    //    var tmp = imageModel;
-    //    return new OutputImageModel() { ImageBytes = imageModel };
+    //    try
+    //    {
+    //        var tmp = Request.Form.Files;
+    //        return new OutputImageModel() { ImageName = tmp.FirstOrDefault()?.FileName ?? "NULL CAME" };
+    //    }
+    //    catch (Exception)
+    //    {
+    //        return new OutputImageModel() { ImageName = "NULL FORM" };
+    //    }
     //}
 }
