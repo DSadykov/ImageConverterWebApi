1f28e7fc7edc8581ae557a8f6615c85fadb736cd
DSadykov <d.sadykov@inbox.ru>
2022-01-17 21:00:30 +0500

    second commit
    first commit

--- ImageConverterWebApi/Program.cs
+++ ImageConverterWebApi/Program.cs
@@ -1,10 +1,12 @@
+using ImageConverterWebApi.Controllers;
+using ImageConverterWebApi.Models;
 using ImageConverterWebApi.Services;
 
 WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-ConfigureServices(builder.Services);
+ConfigureServices(builder.Services, builder.Configuration);
 
 WebApplication? app = builder.Build();
 
@@ -16,22 +18,25 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
+
 app.UseRouting();
 app.UseCors(builder => builder.AllowAnyOrigin());
 app.UseHttpLogging();
 app.UseAuthorization();
 app.MapControllers();
-
+//app.UseMiddleware<MiddlewareTest>();
 app.Urls.Add("http://*:7777");
 
 app.Run();
 
-static void ConfigureServices(IServiceCollection services)
+static void ConfigureServices(IServiceCollection services, ConfigurationManager configurationManager)
 {
     services.AddControllers();
+    services.Configure<ConfigurationModel>(configurationManager.GetSection("MyConfiguration"));
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     services.AddEndpointsApiExplorer();
     services.AddSwaggerGen();
+    services.AddTransient<MiddlewareTest>();
     services.AddTransient<IImageConverter, ImageConverter>();
     services.AddTransient<ImageConverterService, ImageConverterService>();
     services.AddCors();
