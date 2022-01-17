221e75b824f33251f023d4de8e3a2c68059cb369
DSadykov <d.sadykov@inbox.ru>
2022-01-17 21:10:03 +0500

    added configuration model

--- ImageConverterWebApi/Program.cs
+++ ImageConverterWebApi/Program.cs
@@ -1,10 +1,11 @@
+using ImageConverterWebApi.Models;
 using ImageConverterWebApi.Services;
 
 WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-ConfigureServices(builder.Services);
+ConfigureServices(builder.Services, builder.Configuration);
 
 WebApplication? app = builder.Build();
 
@@ -26,10 +27,11 @@ app.Urls.Add("http://*:7777");
 
 app.Run();
 
-static void ConfigureServices(IServiceCollection services)
+static void ConfigureServices(IServiceCollection services, ConfigurationManager configuration)
 {
     services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.Configure<ConfigurationModel>(configuration.GetSection("MyConfiguration"));
     services.AddEndpointsApiExplorer();
     services.AddSwaggerGen();
     services.AddTransient<IImageConverter, ImageConverter>();
