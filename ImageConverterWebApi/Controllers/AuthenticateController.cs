e3420941431664c5978aed46c0182888b3ac5852
DSadykov <d.sadykov@inbox.ru>
2022-01-31 09:55:43 +0500

    added authorization via controllers

---
+++ ImageConverterWebApi/Controllers/AuthenticateController.cs
@@ -0,0 +1,36 @@
+﻿using ImageConverterWebApi.Models;
+using ImageConverterWebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageConverterWebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthenticateController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public AuthenticateController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("authenticate")]
+    public IActionResult Authenticate(AuthenticateRequest model)
+    {
+        AuthenticateResponse? response = _userService.Authenticate(model);
+        return CheckResponse(response);
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(AuthenticateRequest userModel)
+    {
+        AuthenticateResponse? response = await _userService.Register(userModel);
+        return CheckResponse(response);
+    }
+
+    private IActionResult CheckResponse(AuthenticateResponse response)
+    {
+        return response.Token is null ? BadRequest(new { message = response.ErrorMessage }) : Ok(response);
+    }
+}
