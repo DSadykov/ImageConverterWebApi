5b3806870efb2e1b0de6d07a8b4409d7b7c4dd00
DSadykov <d.sadykov@inbox.ru>
2022-02-05 13:16:07 +0500

    moved to mysql

---
+++ ImageConverterWebApi/Migrations/20220204135138_init2.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImageConverterWebApi.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImageModel_Users_UserId",
+                table: "ImageModel");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ImageModel",
+                table: "ImageModel");
+
+            migrationBuilder.RenameTable(
+                name: "ImageModel",
+                newName: "ImageModels");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ImageModel_UserId",
+                table: "ImageModels",
+                newName: "IX_ImageModels_UserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "ImageModels",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FromImageName",
+                table: "ImageModels",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConvertedImageName",
+                table: "ImageModels",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ConvertedImageBytes",
+                table: "ImageModels",
+                type: "longblob",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "BLOB");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContentType",
+                table: "ImageModels",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ImageModels",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ImageModels",
+                table: "ImageModels",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImageModels_Users_UserId",
+                table: "ImageModels",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImageModels_Users_UserId",
+                table: "ImageModels");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ImageModels",
+                table: "ImageModels");
+
+            migrationBuilder.RenameTable(
+                name: "ImageModels",
+                newName: "ImageModel");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ImageModels_UserId",
+                table: "ImageModel",
+                newName: "IX_ImageModel_UserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "ImageModel",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FromImageName",
+                table: "ImageModel",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConvertedImageName",
+                table: "ImageModel",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ConvertedImageBytes",
+                table: "ImageModel",
+                type: "BLOB",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "longblob");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContentType",
+                table: "ImageModel",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ImageModel",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ImageModel",
+                table: "ImageModel",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImageModel_Users_UserId",
+                table: "ImageModel",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- ImageConverterWebApi/Program.cs
+++ ImageConverterWebApi/Program.cs
@@ -1,44 +1,77 @@
+using System.Net;
 using ImageConverterWebApi.Models;
 using ImageConverterWebApi.Services;
 using ImageConverterWebApi.Services.DB;
+using MailKit.Net.Smtp;
 using Microsoft.EntityFrameworkCore;
+using MimeKit;
 
-WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-ConfigureServices(builder.Services, builder.Configuration);
-
-WebApplication? app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+    // Add services to the container.
+
+    ConfigureServices(builder.Services, builder.Configuration);
+
+    WebApplication? app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+    }
+
+    //app.UseHttpsRedirection();
+    app.UseDeveloperExceptionPage();
+    app.UseRouting();
+    app.UseCors(builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+    app.UseHttpLogging();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.UseMiddleware<LoggingMiddleware>();
+    app.UseMiddleware<JwtMiddleware>();
+    //if (!app.Environment.IsDevelopment())
+    //{
+    //    app.Urls.Add("http://*:7777");
+    //}
+    app.Run();
 }
+catch (Exception e)
+{
 
-//app.UseHttpsRedirection();
-app.UseRouting();
-app.UseCors(builder => builder.AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader());
-app.UseHttpLogging();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
-app.UseMiddleware<LoggingMiddleware>();
-app.UseMiddleware<JwtMiddleware>();
-app.Urls.Add("http://*:7777");
-
-app.Run();
-
+    var message = new string[] { e.Message, e?.StackTrace, e?.InnerException?.Message };
+    var mailMessage = new MimeMessage();
+    mailMessage.From.Add(new MailboxAddress("from name", "d.sadykov@inbox.ru"));
+    mailMessage.To.Add(new MailboxAddress("to name", "d.sadykov@inbox.ru"));
+    mailMessage.Subject = "subject";
+    mailMessage.Body = new TextPart("plain")
+    {
+        Text = string.Join('\n', message)
+    };
+    using (var smtpClient = new SmtpClient())
+    {
+        smtpClient.Connect("smtp.mail.ru", 465, true);
+        smtpClient.Authenticate("d.sadykov@inbox.ru", "CittJTRitVs1DtX394kP");
+        smtpClient.Send(mailMessage);
+        smtpClient.Disconnect(true);
+    }
+    throw;
+}
 static void ConfigureServices(IServiceCollection services, ConfigurationManager configuration)
 {
     services.AddCors();
     services.AddControllers();
     var connectionString = configuration.GetConnectionString("DefaultConnection");
-    services.AddDbContext<UsersDBContext>(options => options.UseSqlite(connectionString));
+    services.AddDbContext<UsersDBContext>(options => options.UseMySql(connectionString,
+        new MySqlServerVersion(new Version(10, 5, 10))));
     services.Configure<ConfigurationModel>(configuration.GetSection("MyConfiguration"));
     services.AddEndpointsApiExplorer();
     services.AddSwaggerGen();
