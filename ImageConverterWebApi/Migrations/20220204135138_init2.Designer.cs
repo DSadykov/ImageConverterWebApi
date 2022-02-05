eec5549dd3e54dcd27567af7ae72c35410d8a229
Denis <58462600+DSadykov@users.noreply.github.com>
2022-02-05 16:13:53 +0500

    Feature/move to mysql (#7)
    * moved to mysql

--- ImageConverterWebApi/Migrations/20220204135138_init2.Designer.cs
+++ ImageConverterWebApi/Migrations/20220204135138_init2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ImageConverterWebApi.Services.DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ImageConverterWebApi.Migrations
 {
     [DbContext(typeof(UsersDBContext))]
-    partial class UsersDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220204135138_init2")]
+    partial class init2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
